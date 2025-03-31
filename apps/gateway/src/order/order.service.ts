74309f343b0a861949cf7bd8e2dffd4a1e917d19
minhuits <a41417144@gmail.com>
2025-03-31 19:45:27 +0900

    part 07-4-4. GatewayMicroservice 작업하기

--- apps/gateway/src/order/order.service.ts
+++ apps/gateway/src/order/order.service.ts
@@ -1,19 +1,25 @@
-import { ORDER_SERVICE, UserMeta, UserPayloadDto } from '@app/common';
-import { Inject, Injectable } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ORDER_SERVICE, OrderMicroservice, UserPayloadDto } from '@app/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
 import { CreateOrderDto } from './dto/create.order.dto';
 
 @Injectable()
-export class OrderService {
+export class OrderService implements OnModuleInit {
+    orderService: OrderMicroservice.OrderServiceClient;
+
     constructor(
         @Inject(ORDER_SERVICE)
-        private readonly orderMicroservice: ClientProxy
+        private readonly orderMicroservice: ClientGrpc,
     ) { }
 
+    onModuleInit() {
+        this.orderService = this.orderMicroservice.getService<OrderMicroservice.OrderServiceClient>(
+            'OrderService',
+        )
+    }
+
     async createOrder(createOrderDto: CreateOrderDto, userPayload: UserPayloadDto) {
-        return this.orderMicroservice.send<any, CreateOrderDto & UserMeta>({
-            cmd: 'create_order'
-        }, {
+        return this.orderService.createOrder({
             ...createOrderDto,
             meta: {
                 user: userPayload,
